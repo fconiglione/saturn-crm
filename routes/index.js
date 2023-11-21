b4f86eef0b6ead6ff98140574f887c551aadc67a
fconiglione <122475578+fconiglione@users.noreply.github.com>
2023-11-21 11:25:18 -0500

    added a default homescreen, established a db connection, no functionality yet

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../server/inc/db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Saturn CRM' });
+    res.render('index', { title: 'Saturn CRM' });
 });
 
 module.exports = router;
