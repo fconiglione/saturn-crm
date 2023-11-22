23ea9fa27e2f79fde9397f95e5ae8b69f6d80494
fconiglione <122475578+fconiglione@users.noreply.github.com>
2023-11-21 22:37:15 -0500

    removed sample_data from view

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var sampleRouter = require('./routes/sample_data');
+// var sampleRouter = require('./routes/sample_data');
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/sample_data', sampleRouter);
+// app.use('/sample_data', sampleRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
