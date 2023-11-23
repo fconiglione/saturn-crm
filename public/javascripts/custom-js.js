773bda41ee6fdbcc47035fd66857a42f00e9d681
fconiglione <122475578+fconiglione@users.noreply.github.com>
2023-11-23 15:26:46 -0500

    fixed the app launcher menu with scroll effects and overlay

--- public/javascripts/custom-js.js
+++ public/javascripts/custom-js.js
@@ -19,13 +19,37 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 function open_app_launcher() {
     var appLauncher = document.getElementById("app-launcher");
+    var overlay = document.getElementById("overlay");
+
     appLauncher.style.display = "block";
     appLauncher.classList.remove("inactive");
     appLauncher.classList.add("active");
+    document.getElementById("overlay").style.display = "block";
+
+    overlay.style.display = "block";
+    overlay.addEventListener("click", close_app_launcher);
+    document.body.style.overflow = "hidden";
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
 }
 
 function close_app_launcher() {
     var appLauncher = document.getElementById("app-launcher");
+    var overlay = document.getElementById("overlay");
+
     appLauncher.classList.remove("active");
     appLauncher.classList.add("inactive");
+    document.getElementById("overlay").style.display = "none";
+
+    overlay.style.display = "none";
+    overlay.removeEventListener("click", close_app_launcher);
+    document.body.style.overflow = "auto";
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
 }
